27eef3479f62dd83e85bd3fd2f2dc630298c8fd8
강민지 <gangminji@gangminjiui-MacBookAir.local>
2022-09-28 23:23:06 +0900

    light

--- components/MobileNav.js
+++ components/MobileNav.js
@@ -21,7 +21,7 @@ const MobileNav = () => {
     <div className="sm:hidden">
       <button
         type="button"
-        className="w-8 h-8 ml-1 rounded"
+        className="w-8 h-8 p-0.5 rounded"
         aria-label="Toggle Menu"
         onClick={onToggleNav}
       >
