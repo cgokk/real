c335bac26e1c60ecaee526dad778a75d4d6e967b
강민지 <gangminji@gangminjiui-MacBookAir.local>
2022-09-28 15:39:21 +0900

    real

---
+++ data/authors/default.md
@@ -0,0 +1,9 @@
+---
+name: cgok
+avatar: /static/images/프로필사진.png
+occupation: Frontend Developer cgok
+email: rkdezen@gmail.com
+github: https://github.com/cgokk
+---
+
+노래 시작했다 노래 끝났다.
---
+++ data/siteMetadata.js
@@ -0,0 +1,30 @@
+const siteMetadata = {
+  title: 'cgok blog',
+  author: 'cgok',
+  headerTitle: 'cgok',
+  description: '기획과 개발을 배워요',
+  language: 'ko-KR',
+  siteUrl: 'https://cgokklog.vercel.com',
+  siteRepo: 'https://github.com/cgokk/cgokklog',
+  siteLogo: '/static/images/로고이름.png',
+  image: '/static/images/프로필사진.png',
+  socialBanner: '/static/images/social-banner.png',
+  email: 'rkdezen@gmail.com',
+  github: 'https://github.com/cgokk',
+  locale: 'ko-KR',
+  analytics: {
+    googleAnalyticsId: '',
+  },
+  comment: {
+    provider: 'utterances',
+    utterancesConfig: {
+      repo: 'cgokk/cgokklog',
+      issueTerm: 'title',
+      label: 'Comment 💬',
+      theme: 'github-light',
+      darkTheme: 'github-dark',
+    },
+  },
+}
+
+module.exports = siteMetadata
