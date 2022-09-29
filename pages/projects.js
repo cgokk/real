90551681ceb8989c9b209c5b701770606d2176d5
강민지 <gangminji@gangminjiui-MacBookAir.local>
2022-09-29 20:06:39 +0900

    pb-4

--- pages/projects.js
+++ pages/projects.js
@@ -8,7 +8,7 @@ export default function Projects() {
     <>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="pt-5 md:pt-6 pb-6 md:pb-8 space-y-2 md:space-y-5">
+        <div className="pt-4 md:pt-6 pb-4 md:pb-8 space-y-2 md:space-y-5">
           {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
           <h1 className="text-2xl md:text-4xl font-bold leading-8 md:leading-10 tracking-tight text-gray-900 dark:text-gray-100">
             디자인 이즈 마이 라이프
