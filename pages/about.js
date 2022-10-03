c941132d74b61025a2fdc7e78df17e246dcbf517
강민지 <gangminji@gangminjiui-MacBookAir.local>
2022-10-03 17:21:08 +0900

    re

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function Projects() {
         <div className="pt-4 md:pt-6 pb-4 md:pb-8 space-y-2 md:space-y-5">
           {/* eslint-disable-next-line jsx-a11y/accessible-emoji */}
           <h1 className="text-2xl md:text-4xl font-bold leading-8 md:leading-10 tracking-tight text-gray-900 dark:text-gray-100">
-            소중한 하루를 기록합니다
+            소소한 하루를 기록합니다
           </h1>
         </div>
         <div className="divide-y divide-gray-200 dark:divide-gray-700">
